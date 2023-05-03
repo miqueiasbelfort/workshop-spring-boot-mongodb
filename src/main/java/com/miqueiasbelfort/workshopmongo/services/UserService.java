8be5af38b92f1875d78fc65e76315d5544537d3a
miqueiasbelfort <kawasousa1554@gmail.com>
2023-05-03 16:00:28 -0300

    Insert user with post

--- src/main/java/com/miqueiasbelfort/workshopmongo/services/UserService.java
+++ src/main/java/com/miqueiasbelfort/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.miqueiasbelfort.workshopmongo.domain.User;
+import com.miqueiasbelfort.workshopmongo.dto.UserDTO;
 import com.miqueiasbelfort.workshopmongo.repository.UserRepository;
 import com.miqueiasbelfort.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 	
 }
