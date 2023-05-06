74683fea2d307ba3dc3bd084ef3f88eae28a2128
miqueiasbelfort <kawasousa1554@gmail.com>
2023-05-06 09:46:34 -0300

    endpoint get a post by id

---
+++ src/main/java/com/miqueiasbelfort/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.miqueiasbelfort.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.miqueiasbelfort.workshopmongo.domain.Post;
+import com.miqueiasbelfort.workshopmongo.repository.PostRepository;
+import com.miqueiasbelfort.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found!"));
+	}
+	
+}
