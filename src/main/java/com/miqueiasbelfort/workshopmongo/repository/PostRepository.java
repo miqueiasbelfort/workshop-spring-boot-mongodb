92a3cfaab78a36b3eeba7e1247b9d7b9f82b501a
miqueiasbelfort <kawasousa1554@gmail.com>
2023-05-06 10:23:29 -0300

    simple search with @Query

--- src/main/java/com/miqueiasbelfort/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/miqueiasbelfort/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.miqueiasbelfort.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.miqueiasbelfort.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import com.miqueiasbelfort.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }
